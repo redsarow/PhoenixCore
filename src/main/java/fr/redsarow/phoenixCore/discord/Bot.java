e6d3b15b51a8116a5d92e7b390819dc391ac758d
redsarow <redsarow@users.noreply.github.com>
2020-12-13 18:03:21 +0100

    init discord bot

---
+++ src/main/java/fr/redsarow/phoenixCore/discord/Bot.java
@@ -0,0 +1,12 @@
+package fr.redsarow.phoenixCore.discord;
+
+import discord4j.core.GatewayDiscordClient;
+
+/**
+ * @author redsarow
+ */
+public class Bot {
+
+    private static GatewayDiscordClient client;
+
+}
