a3fcb6a7f5af23d418c10357f9ed3192482278f7
redsarow <redsarow@users.noreply.github.com>
2020-12-13 18:03:35 +0100

    mc Colors

---
+++ src/main/java/fr/redsarow/phoenixCore/minecraft/util/Colors.java
@@ -0,0 +1,16 @@
+package fr.redsarow.phoenixCore.minecraft.util;
+
+import net.minecraft.util.Formatting;
+
+/**
+ * @author redsarow
+ */
+public abstract class Colors {
+
+    public static final Formatting RESET = Formatting.RESET;
+    public static final Formatting ERROR = Formatting.RED;
+    public static final Formatting INFO = Formatting.DARK_AQUA;
+    public static final Formatting WARN = Formatting.GOLD;
+    public static final Formatting OK = Formatting.GREEN;
+
+}
