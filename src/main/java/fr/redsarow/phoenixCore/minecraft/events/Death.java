857cf621f792915d49bbecd400c5bad7a2c02b67
redsarow <redsarow@users.noreply.github.com>
2020-12-30 18:37:20 +0100

    process onDeath

--- src/main/java/fr/redsarow/phoenixCore/minecraft/events/Death.java
+++ src/main/java/fr/redsarow/phoenixCore/minecraft/events/Death.java
@@ -1,7 +1,12 @@
 package fr.redsarow.phoenixCore.minecraft.events;
 
+import fr.redsarow.phoenixCore.PhoenixCore;
+import fr.redsarow.phoenixCore.discord.Bot;
+import fr.redsarow.phoenixCore.minecraft.WorldGroupManager;
+import fr.redsarow.phoenixCore.minecraft.config.configFiles.WorldGroup;
 import fr.redsarow.phoenixCore.minecraft.events.callbacks.ServerPlayerEntityCallback;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.scoreboard.ScoreboardCriterion;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 /**
@@ -15,6 +20,16 @@ public class Death implements ServerPlayerEntityCallback.DeathListener {
 
     @Override
     public void onDeath(ServerPlayerEntity player, DamageSource source) {
-        // TODO
+        String worldName = player.getServerWorld().getRegistryKey().getValue().getPath();
+        WorldGroup.Group worldGroup = WorldGroupManager.getInstance().findGroupByWorldName(worldName);
+        if (!worldGroup.deadCount) {
+            PhoenixCore.getInstance().getServer()
+                    .getScoreboard()
+                    .forEachScore(ScoreboardCriterion.DEATH_COUNT
+                            , player.getEntityName()
+                            , scoreboardPlayerScore ->  scoreboardPlayerScore.incrementScore(-1));
+        }else {
+            Bot.getInstance().ifPresent(bot -> bot.sendMsg.sendDeath(worldName + "\n" + player.getDamageTracker().getDeathMessage().getString()));
+        }
     }
 }
